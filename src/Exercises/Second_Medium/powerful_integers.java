0cb361bf3d58f992ff38646e271ab765225f3ed1
perfecto <nju-zkj@qq.com>
2023-05-02 11:09:54 +0800

    Remove ignored files

---
+++ src/Exercises/Second_Medium/powerful_integers.java
@@ -0,0 +1,16 @@
+package Exercises.Second_Medium;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+//970 强整数
+public class powerful_integers {
+    public List<Integer> powerfulIntegers(int x, int y, int bound) {
+        List<Integer> list = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        int index1 = 0, index2 = 0;
+        return list;
+    }
+}
