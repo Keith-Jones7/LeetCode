0313ea4d320d326d8b51950795eef2dee8e5a606
perfecto <nju-zkj@qq.com>
2023-05-02 16:05:57 +0800

    970 强整数

--- src/Exercises/Second_Medium/powerful_integers.java
+++ src/Exercises/Second_Medium/powerful_integers.java
@@ -8,9 +8,35 @@ import java.util.Set;
 //970 强整数
 public class powerful_integers {
     public List<Integer> powerfulIntegers(int x, int y, int bound) {
-        List<Integer> list = new ArrayList<>();
+        if (bound < 2) {
+            return new ArrayList<>();
+        }
         Set<Integer> set = new HashSet<>();
-        int index1 = 0, index2 = 0;
+        List<Integer> list_x = getPower(x, bound);
+        List<Integer> list_y = getPower(y, bound);
+        for (int num1 : list_x) {
+            for (int num2 : list_y) {
+                int sum = num1 + num2;
+                if (sum > bound) {
+                    break;
+                }else {
+                    set.add(sum);
+                }
+            }
+        }
+        return new ArrayList<>(set);
+    }
+
+    public List<Integer> getPower(int num, int bound) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        if (num != 1) {
+            int power = num;
+            while (power < bound) {
+                list.add(power);
+                power *= num;
+            }
+        }
         return list;
     }
 }
