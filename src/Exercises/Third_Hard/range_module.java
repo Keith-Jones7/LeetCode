c64f5b44adba7ca1db41cdf9317b88642b14d5e3
perfecto <nju-zkj@qq.com>
2023-11-13 14:51:53 +0800

    //715 Range模块

--- src/Exercises/Third_Hard/range_module.java
+++ src/Exercises/Third_Hard/range_module.java
@@ -1,22 +1,48 @@
 package Exercises.Third_Hard;
+import java.util.*;
 //715 Range模块
 public class range_module {
 }
 class RangeModule {
 
-    public RangeModule() {
+    private TreeMap<Integer, Integer> intervals;
 
+    public RangeModule() {
+        intervals = new TreeMap<>();
     }
 
     public void addRange(int left, int right) {
-
+        Integer start = intervals.floorKey(left);
+        if (start == null) start = intervals.ceilingKey(left);
+        while (start != null && start <= right) {
+            int end = intervals.get(start);
+            if (end >= left) {
+                intervals.remove(start);
+                if (start < left) left = start;
+                if (end > right) right = end;
+            }
+            start = intervals.ceilingKey(end);
+        }
+        intervals.put(left, right);
     }
 
     public boolean queryRange(int left, int right) {
-
+        Integer start = intervals.floorKey(left);
+        if (start == null) return false;
+        return intervals.get(start) >= right;
     }
 
     public void removeRange(int left, int right) {
-
+        Integer start = intervals.floorKey(left);
+        if (start == null) start = intervals.ceilingKey(left);
+        while (start != null && start < right) {
+            int end = intervals.get(start);
+            if (end >= left) {
+                intervals.remove(start);
+                if (start < left) intervals.put(start, left);
+                if (end > right) intervals.put(right, end);
+            }
+            start = intervals.ceilingKey(end);
+        }
     }
 }
