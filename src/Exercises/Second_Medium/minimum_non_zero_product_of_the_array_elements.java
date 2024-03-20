b48012a910210b2b589297ddc10173e477a596d0
zkjperfecto <zkjperfecto@gmail.com>
2024-03-20 13:57:34 +0800

    //1969 数组元素的最小非零乘积

---
+++ src/Exercises/Second_Medium/minimum_non_zero_product_of_the_array_elements.java
@@ -0,0 +1,26 @@
+package Exercises.Second_Medium;
+//1969 数组元素的最小非零乘积
+public class minimum_non_zero_product_of_the_array_elements {
+    int MOD = 1_0000;
+    public int minNonZeroProduct(int p) {
+        long n = (1L << p);
+        long ans = quickPow(n - 2, (n - 2) / 2) * (n - 1) % MOD;
+        return (int) ans;
+    }
+
+    public long quickPow(long a, long b) {
+        a %= MOD;
+        if (b == 0) {
+            return 1;
+        }
+        if (b == 1) {
+            return a;
+        }
+        long ans = quickPow(a, b / 2);
+        ans = (ans * ans) % MOD;
+        if (b % 2 == 1) {
+            ans = (ans * a) % MOD;
+        }
+        return ans;
+    }
+}
