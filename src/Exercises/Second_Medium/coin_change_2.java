cfa9346acde913b544baf5ae86668c0f9b8c62c4
zkjperfecto <zkjperfecto@gmail.com>
2024-03-25 11:17:01 +0800

    //518 零钱兑换2

---
+++ src/Exercises/Second_Medium/coin_change_2.java
@@ -0,0 +1,18 @@
+package Exercises.Second_Medium;
+
+import java.util.Arrays;
+import java.util.*;
+
+//518 零钱兑换2
+public class coin_change_2 {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+}
